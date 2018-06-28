b0bed9fc618a3cb0b79222da159e755bc0875c68
Latifat Ozoya <latifat@fb.com>
2018-06-28 16:05:03 -0700

    included portrait mode effects

--- app/src/main/java/com/app/latifat/flicks/models/Config.java
+++ app/src/main/java/com/app/latifat/flicks/models/Config.java
@@ -10,6 +10,8 @@ public class Config {
     String imageBasedUrl;
     //the poster size to use when fetching images, part of the URL
     String posterSize;
+    //the backdrop size to use when fetching images
+    String backdropSize;
 
     public Config(JSONObject object) throws JSONException {
 
@@ -20,6 +22,9 @@ public class Config {
         JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
         // use the option at index 3 or w342 as a fallback
         posterSize = posterSizeOptions.optString(3, "w342");
+        //parse the backdrop sizes and use the option at index 1 or w780 as a fallback
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
 
     }
 
@@ -34,5 +39,9 @@ public class Config {
 
     public String getPosterSize() {
         return posterSize;
+    }
+
+    public String getBackdropSize() {
+        return backdropSize;
     }
 }
